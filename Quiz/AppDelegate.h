ca18bf0c199fac3ac30bead3fcdf98f6ae55124e
Ralph Gabuat <ralph.gabuat@fox.com>
2015-07-20 09:40:58 -0700

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Student on 7/20/15.
+//  Copyright (c) 2015 Student_Ralph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
